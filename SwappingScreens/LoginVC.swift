7949675f500f9821628e562bb938f1cda20a8043
Jschloss309 <Jschloss309@gmail.com>
2016-10-22 20:10:01 -0600

    Initial commit

---
+++ SwappingScreens/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwappingScreens
+//
+//  Created by Jonathon Schloss on 10/22/16.
+//  Copyright © 2016 Jonathon Schloss. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
